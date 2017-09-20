fcff696610283e26d9d745693d23f08518333a21
Andy Coenen <andrew.coenen@gmail.com>
2017-09-19 19:45:14 -0700

    Use synthetic default import for React

--- app/components/VerticalGrid/index.tsx
+++ app/components/VerticalGrid/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import { range } from 'lodash'
 
 const styles = require('./styles.less')
--- app/features/Timeline/index.tsx
+++ app/features/Timeline/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import { range } from 'lodash'
 import { inject, observer } from 'mobx-react'
 
