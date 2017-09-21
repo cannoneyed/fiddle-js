36f75dc55c5baed5d9ac8b9cd6033af3a399b09d
Andy Coenen <andrew.coenen@gmail.com>
2017-09-20 19:29:13 -0700

    Continue on new layout system

--- app/features/Tracks/index.tsx
+++ app/features/Tracks/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
 
+import VerticalGrid from 'components/VerticalGrid'
 import Track from 'features/Track'
 
 import trackStore, { TrackStore } from 'core/stores/tracks'
+import sequencerViewStore, { SequencerViewStore } from 'core/stores/sequencer/view'
 
 const styles = require('./styles.less')
 
@@ -11,10 +13,12 @@ interface ComponentProps {}
 
 interface InjectedProps extends ComponentProps {
   trackStore: TrackStore
+  sequencerViewStore: SequencerViewStore
 }
 
 @inject(() => ({
   trackStore,
+  sequencerViewStore,
 }))
 @observer
 export default class TracksContainer extends React.Component<ComponentProps, {}> {
@@ -23,11 +27,13 @@ export default class TracksContainer extends React.Component<ComponentProps, {}>
   }
 
   render() {
-    const { trackStore } = this.injected
+    const { sequencerViewStore, trackStore } = this.injected
     const { trackList } = trackStore
+    const { gridCount, gridSegmentWidth } = sequencerViewStore
 
     return (
-      <div className={styles.tracksWrapper}>
+      <div className={styles.tracksAreaContainer}>
+        <VerticalGrid gridCount={gridCount} gridSegmentWidth={gridSegmentWidth} />
         <div className={styles.tracksContainer} id="tracksContainer">
           {trackList.map((track, index) => <Track track={track} index={index} key={index} />)}
         </div>
