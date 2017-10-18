f16b0d784025b40bfbf4fea4f036c4fdaaa8a795
Andy Coenen <andy@patreon.com>
2017-10-17 21:46:08 -0700

    basic on off drag to position marker

---
+++ app/components/Caret/index.tsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+interface ComponentProps {
+  size?: number
+  fillColor?: string
+  strokeColor?: string
+  strokeWidth?: number
+}
+
+export default class Caret extends Component<ComponentProps, {}> {
+  static defaultProps = {
+    size: 10,
+    fillColor: 'black',
+    strokeColor: 'black',
+    strokeWidth: 1,
+  }
+
+  render() {
+    const { size, fillColor, strokeColor, strokeWidth } = this.props
+
+    const svgStyle = {
+      width: size,
+      height: size,
+    }
+
+    const polygonStyle = {
+      fill: fillColor,
+      stroke: strokeColor,
+      strokeWidth,
+    }
+
+    return (
+      <svg style={svgStyle} viewBox="0 0 100 100">
+        <polygon style={polygonStyle} points="0,0 100,0 50,75" />
+      </svg>
+    )
+  }
+}
