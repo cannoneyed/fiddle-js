1c904d663b01371038242b52fb96a4595a3f06c8
Andy Coenen <andrew.coenen@gmail.com>
2018-04-18 17:16:33 -0700

    Make TimelineLayout injectable

--- app/core/layouts/sequencer/tracks/index.ts
+++ app/core/layouts/sequencer/tracks/index.ts
@@ -1,14 +1,14 @@
 import { Container, Service } from 'typedi';
 
 import { GridLayout } from './grid';
-import { timelineLayout } from './timeline';
+import { TimelineLayout } from './timeline';
 import { tracksLayout } from './tracks';
 import { zoomLayout } from './zoom';
 
 @Service()
 export class TracksSectionLayout {
   grid = Container.get(GridLayout);
-  timeline = timelineLayout;
+  timeline = Container.get(TimelineLayout);
   tracks = tracksLayout;
   zoom = zoomLayout;
 }
--- app/core/layouts/sequencer/tracks/timeline.ts
+++ app/core/layouts/sequencer/tracks/timeline.ts
@@ -1,12 +1,12 @@
+import { Service } from 'typedi';
 import { computed } from 'mobx';
 
 import { clipDragInteraction } from 'core/interactions/clip/drag';
 
+@Service()
 export class TimelineLayout {
   @computed
   get dropTargetPosition() {
     return clipDragInteraction.isDragging ? clipDragInteraction.dropTargetPosition : null;
   }
 }
-
-export const timelineLayout = new TimelineLayout();
