3d0bcf488a16f618cda6139018907c9a4ab75f67
Andy Coenen <andrew.coenen@gmail.com>
2018-04-20 16:48:08 -0700

    Remove unused utils

--- app/utils/connect.ts
+++
@@ -1,8 +0,0 @@
-import { inject } from 'mobx-react';
-
-export function connect<T, K extends keyof T>(
-  ComponentClass: React.ComponentType<T>,
-  ...stores: K[]
-): React.ComponentType<Pick<T, Exclude<keyof T, K>> & Partial<Pick<T, K>>> {
-  return inject(...stores)(ComponentClass);
-}
--- app/utils/log-stores.ts
+++ app/utils/log-stores.ts
@@ -1,10 +1,7 @@
 import { Container } from 'typedi';
 
-import * as stores from 'core/stores';
-
 export const logStores = () => {
   (window as any).logStores = () => ({
-    stores,
     Container,
   });
 };
