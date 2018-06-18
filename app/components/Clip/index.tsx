b47c9d88734953aba47010368f8bd3991996c45e
Andy Coenen <andrew.coenen@gmail.com>
2018-06-17 23:42:08 -0700

    Finish cleanup of inject

--- app/components/Clip/index.tsx
+++ app/components/Clip/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import styled from 'styled-components';
 import { Container } from 'typedi';
 import { observer } from 'mobx-react';
+import { injector } from 'utils/injector';
 
 import { Clip as ClipModel } from 'core/models/clip';
 import { TracksLayout } from 'core/state/layouts/sequencer/tracks';
@@ -12,22 +13,32 @@ interface Props {
   onMouseDown?: (event: React.MouseEvent<HTMLElement>) => any;
   isDragging?: boolean;
 }
+interface InjectedProps {
+  height: number;
+  width: number;
+}
+
+const inject = injector<Props, InjectedProps>(props => {
+  const tracksLayout = Container.get(TracksLayout);
+  const sequencerPosition = Container.get(SequencerPositionService);
+
+  const width = sequencerPosition.getWidth(props.clip.length);
+  return {
+    height: tracksLayout.trackHeight,
+    width,
+  };
+});
 
 @observer
-export default class Clip extends React.Component<Props, {}> {
-  tracksLayout = Container.get(TracksLayout);
-  sequencerPosition = Container.get(SequencerPositionService);
-
+export class Clip extends React.Component<Props & InjectedProps, {}> {
   render() {
-    const { clip, onMouseDown } = this.props;
-    const { trackHeight } = this.tracksLayout;
-    const clipWidth = this.sequencerPosition.getWidth(clip.length);
+    const { clip, onMouseDown, height, width } = this.props;
 
     return (
       <ClipContainer
         id={clip.domId}
-        height={trackHeight}
-        width={clipWidth}
+        height={height}
+        width={width}
         isSelected={clip.isSelected}
         isDragging={clip.isDragging}
         onMouseDown={onMouseDown}
@@ -35,6 +46,8 @@ export default class Clip extends React.Component<Props, {}> {
     );
   }
 }
+
+export default inject(Clip);
 
 export interface ClipContainerProps {
   height: number;
