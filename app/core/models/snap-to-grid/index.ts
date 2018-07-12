59551b97c796011358df4a9dcd85af42613efcb2
Andy Coenen <andrew.coenen@gmail.com>
2018-07-11 17:31:23 -0700

    Use fractions in snap to grid

--- app/core/models/snap-to-grid/index.ts
+++ app/core/models/snap-to-grid/index.ts
@@ -1,16 +1,18 @@
 import { action, computed, observable } from 'mobx';
 
+import { Fraction } from 'core/primitives/fraction';
+
 export class SnapToGridValue {
-  constructor(public name: string, public divisions: number) {}
+  constructor(public name: string, public division: Fraction) {}
 }
 
 export class SnapToGridValues {
-  readonly snap_1 = new SnapToGridValue('1 bar', 1);
-  readonly snap_1_2 = new SnapToGridValue('1/2', 2);
-  readonly snap_1_4 = new SnapToGridValue('1/4', 4);
-  readonly snap_1_8 = new SnapToGridValue('1/8', 8);
-  readonly snap_1_16 = new SnapToGridValue('1/16', 16);
-  readonly snap_1_32 = new SnapToGridValue('1/32', 32);
+  readonly snap_1 = new SnapToGridValue('1 bar', new Fraction(1, 1));
+  readonly snap_1_2 = new SnapToGridValue('1/2', new Fraction(1, 2));
+  readonly snap_1_4 = new SnapToGridValue('1/4', new Fraction(1, 4));
+  readonly snap_1_8 = new SnapToGridValue('1/8', new Fraction(1, 8));
+  readonly snap_1_16 = new SnapToGridValue('1/16', new Fraction(1, 16));
+  readonly snap_1_32 = new SnapToGridValue('1/32', new Fraction(1, 32));
   readonly [key: string]: SnapToGridValue;
 }
 
@@ -29,11 +31,7 @@ export class SnapToGrid {
   }
 
   @computed
-  get divisions() {
-    return this.value.divisions;
-  }
-
-  getDivisionWidth(barWidth: number) {
-    return barWidth / this.value.divisions;
+  get division() {
+    return this.value.division;
   }
 }
