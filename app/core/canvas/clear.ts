4c9f21a5e9d651b7574a173363f697199b3cd5dc
Andy Coenen <andrew.coenen@gmail.com>
2018-08-06 20:43:09 -0700

    Remove unused canvas methods

--- app/core/canvas/clear.ts
+++
@@ -1,9 +0,0 @@
-export const clear = (
-  ctx: CanvasRenderingContext2D,
-  x: number,
-  y: number,
-  w: number,
-  h: number
-) => {
-  ctx.clearRect(x, y, w, h);
-};
--- app/core/canvas/index.ts
+++
@@ -1,4 +0,0 @@
-export { clear } from './clear';
-export { drawHorizontalLine, drawVerticalLine } from './draw-lines';
-export { drawRectangle } from './draw-shapes';
-export { resizeCanvas } from './resize';
