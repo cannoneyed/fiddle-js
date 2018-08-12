5f376c3e3ad9466f51883290185e0715983b8a4b
Andy Coenen <andrew.coenen@gmail.com>
2018-08-11 19:51:36 -0700

    Update core

--- app/features/NotesEditor/core/index.ts
+++ app/features/NotesEditor/core/index.ts
@@ -1,9 +1,11 @@
-import { createMandatoryContext, makeCoreInjector } from 'utils/context';
+import { action } from 'mobx';
 import { NotesEditorLayout } from './layout';
 
 import { Notes } from 'core/models/notes';
 import { KeyLayout, Piano88 } from 'core/models/notes/key-layout';
 import { SnapToGrid } from 'core/models/snap-to-grid';
+
+import { Props } from 'features/NotesEditor';
 
 export class NotesEditorCore {
   constructor(public notes: Notes) {}
@@ -12,9 +14,19 @@ export class NotesEditorCore {
 
   snapToGrid = new SnapToGrid();
   keyLayout: KeyLayout = new Piano88();
+
+  @action
+  updateFromProps(props: Props) {
+    this.layout.dimensions = props.dimensions;
+    this.layout.rowHeight = props.rowHeight;
+    this.keyLayout = props.keyLayout;
+    this.snapToGrid = props.snapToGrid;
+  }
 }
 
-const { Provider, Consumer } = createMandatoryContext<NotesEditorCore>();
-export { Provider, Consumer };
-
-export const injectCore = makeCoreInjector(Consumer);
+const coreRegister = new Map<Notes, NotesEditorCore>();
+export function getCore(notes: Notes) {
+  const core = coreRegister.has(notes) ? coreRegister.get(notes)! : new NotesEditorCore(notes);
+  coreRegister.set(notes, core);
+  return core;
+}
