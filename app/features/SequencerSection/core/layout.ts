16cb392260b14aab9bc3741cd698278bf8f7ac9e
Andy Coenen <andrew.coenen@gmail.com>
2018-08-12 16:44:33 -0700

    Update the sequencer core layout

--- app/features/SequencerSection/core/layout.ts
+++ app/features/SequencerSection/core/layout.ts
@@ -1,28 +1,13 @@
 import { computed, observable } from 'mobx';
-import { Container } from 'typedi';
 
 import { Dimensions, Position, Rectangle } from 'core/interfaces';
 
-import { MainPageLayout } from 'core/state/layouts/pages/main';
-
 export class SequencerLayout {
-  mainPageLayout = Container.get(MainPageLayout);
-
   @observable
   dimensions = {
     height: 0,
     width: 0,
   };
-
-  @computed
-  get sectionHeight() {
-    return this.dimensions.height;
-  }
-
-  @computed
-  get sectionWidth() {
-    return this.dimensions.width;
-  }
 
   @observable
   minimapHeight = 30;
@@ -40,41 +25,42 @@ export class SequencerLayout {
   verticalScrollbarWidth = 14;
 
   @computed
-  get tracksAreaDimensions(): Dimensions {
+  get tracksStageDimensions(): Dimensions {
+    const { dimensions } = this;
     return {
-      height: this.sectionHeight - this.toolbarHeight - this.minimapHeight - this.timelineHeight,
-      width: this.mainPageLayout.sectionWidth - this.verticalScrollbarWidth,
+      height: dimensions.height - this.toolbarHeight - this.minimapHeight,
+      width: dimensions.width - this.gutterWidth - this.verticalScrollbarWidth,
     };
   }
 
   @computed
-  get tracksAreaPosition(): Position {
+  get tracksStagePosition(): Position {
     return {
       left: this.gutterWidth,
+      top: this.toolbarHeight + this.minimapHeight,
+    };
+  }
+
+  @computed
+  get tracksDimensions(): Dimensions {
+    return {
+      height: this.tracksStageDimensions.height - this.timelineHeight,
+      width: this.tracksStageDimensions.width,
+    };
+  }
+
+  @computed
+  get tracksPosition(): Position {
+    return {
       top: this.toolbarHeight + this.minimapHeight + this.timelineHeight,
-    };
-  }
-
-  @computed
-  get gridDimensions(): Dimensions {
-    return {
-      height: this.tracksAreaDimensions.height,
-      width: this.tracksAreaDimensions.width - this.gutterWidth,
-    };
-  }
-
-  @computed
-  get gridPosition(): Position {
-    return {
-      top: this.tracksAreaPosition.top,
-      left: this.gutterWidth,
+      left: 0,
     };
   }
 
   @computed
   get verticalScrollbarDimensions(): Dimensions {
     return {
-      height: this.sectionHeight - this.toolbarHeight - this.minimapHeight,
+      height: this.tracksStageDimensions.height,
       width: this.verticalScrollbarWidth,
     };
   }
@@ -82,18 +68,13 @@ export class SequencerLayout {
   @computed
   get verticalScrollbarPosition(): Position {
     return {
-      left: 0,
-      top: this.toolbarHeight + this.minimapHeight,
+      left: this.gutterWidth + this.tracksDimensions.width,
+      top: 0,
     };
   }
 
   @computed
   get verticalScrollbarRectangle(): Rectangle {
     return { ...this.verticalScrollbarDimensions, ...this.verticalScrollbarPosition };
-  }
-
-  @computed
-  get tracksWidth() {
-    return this.tracksAreaDimensions.width - this.gutterWidth;
   }
 }
