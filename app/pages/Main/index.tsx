15aee2c47036d1613c472c91651ec3a53c813a1a
Andy Coenen <andrew.coenen@gmail.com>
2018-09-28 21:13:20 -0700

    Add clipEditor back

--- app/pages/Main/index.tsx
+++ app/pages/Main/index.tsx
@@ -9,6 +9,7 @@ import { Dimensions } from 'core/interfaces';
 
 import Sequencer from 'features/Sequencer';
 import SectionDivider from 'features/SectionDivider';
+import ClipEditor from 'features/ClipEditorSection';
 
 import { MainPageLayout } from 'core';
 
@@ -35,12 +36,13 @@ export class MainPage extends React.Component<Props & InjectedProps, {}> {
   };
 
   render() {
-    const { sequencerDimensions } = this.props;
+    const { clipEditorDimensions, sequencerDimensions } = this.props;
 
     return (
       <PageWrapper>
         <Sequencer dimensions={sequencerDimensions} />
         <SectionDivider onDrag={this.handleSectionDividerDrag} />
+        <ClipEditor dimensions={clipEditorDimensions} />
       </PageWrapper>
     );
   }
